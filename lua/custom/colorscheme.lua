2a200e6c3e28252c6facd96c4bb0706451725b81
JPatrik <jokhel.roland@gmail.com>
2026-06-25 20:08:49 +0200

    feat: update catppuccin-nvim colorscheme

--- lua/custom/colorscheme.lua
+++ lua/custom/colorscheme.lua
@@ -1,14 +1,14 @@
 return {
-  {
-    dir = vim.fn.expand '~/Development/projects/cyberpunk.nvim',
-    enabled = false,
-    name = 'cyberpunk',
-    dev = { true },
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'cyberpunk'
-    end,
-  },
+  -- {
+  --   dir = vim.fn.expand '~/Development/projects/cyberpunk.nvim',
+  --   enabled = false,
+  --   name = 'cyberpunk',
+  --   dev = { true },
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'cyberpunk'
+  --   end,
+  -- },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
@@ -16,6 +16,11 @@ return {
     enabled = true,
     config = function()
       require('catppuccin').setup {
+        auto_integrations = true,
+        integrations = {
+          treesitter = true,
+          nvimtree = true,
+        },
         flavour = 'mocha',
         float = {
           transparent = true,
@@ -32,15 +37,15 @@ return {
           end,
         },
       }
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'catppuccin-nvim'
     end,
   },
-  {
-    'Lambozhuang/cyberpunk.nvim',
-    enabled = false,
-    priority = 1000,
-    opts = {
-      transparent = true, -- set to true for transparent background
-    },
-  },
+  -- {
+  --   'Lambozhuang/cyberpunk.nvim',
+  --   enabled = false,
+  --   priority = 1000,
+  --   opts = {
+  --     transparent = true, -- set to true for transparent background
+  --   },
+  -- },
 }
